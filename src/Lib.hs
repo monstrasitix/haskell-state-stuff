12369097b98ed3a8800d6d463b66623394f3f213
monstrasitix <injectedinferno@gmail.com>
2023-07-26 23:11:04 +0100

    Goodnight

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,27 +1,23 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Lib (startApp) where
 
 import Data.Function
 import Network.Wai.Handler.Warp
 import Servant
-
 import API
 import Database.SQLite.Simple
 import Migration
-
 
 settings :: Settings
 settings = defaultSettings
     & setPort 8080
     & setHost "127.0.0.1"
 
-
 startApp :: IO ()
 startApp = do
-    Migration.run & withConnection "./sqlite.db" 
+    Migration.run & withConnection "./sqlite.db"
     runSettings settings
         $ serve (Proxy @API.API) API.server
